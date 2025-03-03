91bb7484ae535260be376960951cf5db8abaee7f
KittipongChaiya <111034922+KittipongChaiya@users.noreply.github.com>
2025-03-03 09:19:51 +0700

    Fix login demo

--- lib/screens/111.dart
+++ lib/screens/111.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class LoginPage1 extends StatelessWidget {
+  const LoginPage1({super.key});
 
   @override
   Widget build(BuildContext context) {
