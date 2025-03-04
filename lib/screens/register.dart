be4303c0004d9fcc06ec594a7b7be1670a3f00f9
KittipongChaiya <111034922+KittipongChaiya@users.noreply.github.com>
2025-03-04 16:45:36 +0700

    Add check email in register.dart

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -15,6 +15,53 @@ class _RegisterPageState extends State<RegisterPage> {
   final _usernameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  bool _isEmailAvailable = true;
+
+   Future<void> _checkEmailAvailability() async {
+    if (_emailController.text.isEmpty) {
+      return;
+    }
+
+    final url = Uri.parse('http://10.0.2.2:3000/api/auth/check-email');
+    
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({
+          'email': _emailController.text,
+        }),
+      );
+
+      setState(() {
+        _isEmailAvailable = response.statusCode == 200;
+      });
+
+      if (_isEmailAvailable) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            'อีเมลนี้สามารถใช้งานได้',
+            style: GoogleFonts.kanit(),
+          ),
+          backgroundColor: Colors.green,
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            'อีเมลนี้มีอยู่ในระบบแล้ว',
+            style: GoogleFonts.kanit(),
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  } catch (e) {
+    print('Error checking email: $e');
+  }
+}
 
   // Function สำหรับเชื่อมต่อและบันทึกข้อมูลลง PostgreSQL
   Future<void> _registerUser() async {
@@ -69,6 +116,7 @@ class _RegisterPageState extends State<RegisterPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              
               Image.asset(
                 'assets/images/w.png',
                 width: 200,
@@ -90,21 +138,35 @@ class _RegisterPageState extends State<RegisterPage> {
               ),
 
               SizedBox(height: 16.0),
+
               TextFormField(
                 controller: _emailController,
                 decoration: InputDecoration(
                   labelText: 'อีเมล',
                   labelStyle: GoogleFonts.kanit(textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.check),
+                    onPressed: _checkEmailAvailability,
+                  ),
                 ),
+                onChanged: (value) {
+                  setState(() {
+                    _isEmailAvailable = true;
+                  });
+                },
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'กรุณากรอกอีเมล';
+                  }
+                  if (!_isEmailAvailable) {
+                    return 'อีเมลนี้มีอยู่ในระบบแล้ว';
                   }
                   return null;
                 },
               ),
 
               SizedBox(height: 16.0),
+
               TextFormField(
                 controller: _passwordController,
                 obscureText: true,
@@ -121,6 +183,7 @@ class _RegisterPageState extends State<RegisterPage> {
               ),
 
               SizedBox(height: 24),
+
               ElevatedButton(
                 onPressed: () {
                   if (_formKey.currentState!.validate()) {
@@ -133,6 +196,7 @@ class _RegisterPageState extends State<RegisterPage> {
               ),
 
               SizedBox(height: 10),
+              
               ElevatedButton(
                 onPressed: (){
                   Navigator.pushReplacement(context, MaterialPageRoute(
