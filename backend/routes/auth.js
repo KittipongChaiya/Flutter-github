ca01c86937f20b51ac473fe486f51ea5bfb928f7
KittipongChaiya <111034922+KittipongChaiya@users.noreply.github.com>
2025-03-05 11:59:15 +0700

    Add router authenticatetoken

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authenticateToken = require('../authMiddleware');
 
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
 router.post('/check-email', userController.checkEmail);
+const { getUserData } = require('../controllers/userController');
+router.get('/user', authenticateToken, getUserData);
 
 module.exports = router;
