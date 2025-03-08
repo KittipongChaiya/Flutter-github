539858acef7bdd37a2a96e257e66dfd94f6e2c37
KittipongChaiya <111034922+KittipongChaiya@users.noreply.github.com>
2025-03-08 16:40:19 +0700

    Add transaction_provider

--- lib/providers/transaction_provider.dart
+++ lib/providers/transaction_provider.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/foundation.dart';
-import 'package:myproject/models/transaction.dart';
+import 'package:myproject/database/transaction_db.dart';
+import 'package:myproject/models/transactions.dart';
 
 class TransactionProvider with ChangeNotifier{
   //ข้อมูล
-  List<Transaction> transactions = [
+  List<Transactions> transactions = [
     
   ];
 
   //ดึงข้อมูล
-  List<Transaction> getTransaction(){
+  List<Transactions> getTransaction(){
     return transactions;
   }
 
-  void addTransaction(Transaction statement){
-    transactions.insert(0, (statement));
+  void initData() async{
+    var db = TransactionDB(dbName: "transaction.db");
+    //ดึงข้อมูลตอนเริ่มต้น
+    transactions = await db.loadAllData();
+    notifyListeners();
+  }
+
+  void addTransaction(Transactions statement) async{
+    //ปิดไว้ก่อนเพราะเราจะย้านไปใช้ฐานข้อมูล var db = await TransactionDB(dbName: "transaction.db").openDatabase();
+    //ปิดไว้ก่อนเพราะเราจะย้านไปใช้ฐานข้อมูล print(db);
+
+    var db = await TransactionDB(dbName: "transaction.db");
+    //จากนั้นบันทึกข้อมูล
+    await db.InsertData(statement);
+
+    //แสดงข้อมูล
+    transactions = await db.loadAllData();
+
+    //transaction.insert(0, (statement));
     notifyListeners();
   }
 }
